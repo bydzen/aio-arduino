cfdf6f9bbeb29866211244646dacd1f67e5ad5e6
bydzen <svnteentthones@gmail.com>
2022-05-16 17:26:14 +0700

    update delay to 500 ⌛

--- runningled1/runningled1.ino
+++ runningled1/runningled1.ino
@@ -8,11 +8,12 @@ int theLED6 = 7;
 
 int theLED7 = 8;
 
-void setup() {
+void setup()
+{
   pinMode(theLED1, OUTPUT);
   pinMode(theLED2, OUTPUT);
   pinMode(theLED3, OUTPUT);
- 
+
   pinMode(theLED4, OUTPUT);
   pinMode(theLED5, OUTPUT);
   pinMode(theLED6, OUTPUT);
@@ -32,121 +33,126 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   int speedLED = analogRead(A5);
-  
-//  // Blink 
-//  for (int i = 2; i < 9; i++) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED);
-//    digitalWrite(i, LOW);
-//    delay(speedLED);
-//  }
-//
-//  for (int i = 7; i > 2; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED);
-//    digitalWrite(i, LOW);
-//    delay(speedLED);
-//  }
 
-//  // Hold
-//  for (int i = 2; i < 9; i++) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED*2);
-//  }
-//
-//  for (int i = 8; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, LOW);
-//    delay(speedLED*2);
-//  }
-//
-//  // Hold reverse
-//  for (int i = 8; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED*2);
-//  }
-//
-//  for (int i = 2; i < 9; i++) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, LOW);
-//    delay(speedLED*2);
-//  }
+  //  // Blink
+  //  for (int i = 2; i < 9; i++) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED);
+  //    digitalWrite(i, LOW);
+  //    delay(speedLED);
+  //  }
+  //
+  //  for (int i = 7; i > 2; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED);
+  //    digitalWrite(i, LOW);
+  //    delay(speedLED);
+  //  }
+
+  //  // Hold
+  //  for (int i = 2; i < 9; i++) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED*2);
+  //  }
+  //
+  //  for (int i = 8; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, LOW);
+  //    delay(speedLED*2);
+  //  }
+  //
+  //  // Hold reverse
+  //  for (int i = 8; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED*2);
+  //  }
+  //
+  //  for (int i = 2; i < 9; i++) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, LOW);
+  //    delay(speedLED*2);
+  //  }
 
   // Snake start to end
-  for (int i = 2; i < 9; i++) {
+  for (int i = 2; i < 9; i++)
+  {
     int speedLED = analogRead(A5);
     digitalWrite(i, HIGH);
-    if (i == 8) {
-      delay(350);
+    if (i == 8)
+    {
+      delay(500);
       break;
     }
-    delay(speedLED/2);
+    delay(speedLED / 2);
   }
 
-  for (int i = 2; i < 9; i++) {
+  for (int i = 2; i < 9; i++)
+  {
     int speedLED = analogRead(A5);
     digitalWrite(i, LOW);
-    if (i == 8) {
-      delay(350);
+    if (i == 8)
+    {
+      delay(500);
       break;
     }
-    delay(speedLED/2);
+    delay(speedLED / 2);
   }
 
-//  // Snake back reverse start
-//  for (int i = 7; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED/4);
-//    digitalWrite(i, LOW);
-//  }
-//
-//  // Snake next end
-//  for (int i = 2; i < 9; i++) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED/4);
-//    digitalWrite(i, LOW);
-//  }
-//
-//  // Snake reverse
-//  for (int i = 8; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    if (i == 2) {
-//      break;
-//    }
-//    delay(speedLED);
-//  }
-//
-//  for (int i = 8; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, LOW);
-//    if (i == 2) {
-//      break;
-//    }
-//    delay(speedLED);
-//  }
-//
-//  // Snake back reverse end
-//  for (int i = 2; i < 9; i++) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED/4);
-//    digitalWrite(i, LOW);
-//  }
-//
-//  // Snake next start
-//  for (int i = 7; i > 1; i--) {
-//    int speedLED = analogRead(A5);
-//    digitalWrite(i, HIGH);
-//    delay(speedLED/4);
-//    digitalWrite(i, LOW);
-//  }
+  //  // Snake back reverse start
+  //  for (int i = 7; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED/4);
+  //    digitalWrite(i, LOW);
+  //  }
+  //
+  //  // Snake next end
+  //  for (int i = 2; i < 9; i++) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED/4);
+  //    digitalWrite(i, LOW);
+  //  }
+  //
+  //  // Snake reverse
+  //  for (int i = 8; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    if (i == 2) {
+  //      break;
+  //    }
+  //    delay(speedLED);
+  //  }
+  //
+  //  for (int i = 8; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, LOW);
+  //    if (i == 2) {
+  //      break;
+  //    }
+  //    delay(speedLED);
+  //  }
+  //
+  //  // Snake back reverse end
+  //  for (int i = 2; i < 9; i++) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED/4);
+  //    digitalWrite(i, LOW);
+  //  }
+  //
+  //  // Snake next start
+  //  for (int i = 7; i > 1; i--) {
+  //    int speedLED = analogRead(A5);
+  //    digitalWrite(i, HIGH);
+  //    delay(speedLED/4);
+  //    digitalWrite(i, LOW);
+  //  }
 }
